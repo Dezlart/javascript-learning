731f20633dd9932159d5ebbc39ee6f389de2e21b
Dezlart <pbelokopytin@gmail.com>
2026-09-08 00:17:28 +0300

    arrays

---
+++ exercises/metanit/chapter-02/Introduction-to-Arrays.js
@@ -0,0 +1,13 @@
+const colors = ["Красный", "Зеленый", "Синий"];
+console.log(colors[0]);
+console.log(colors[2]);
+colors[1] = "Желтый";
+console.log(colors);
+
+const matrix = [
+    [1, 2],
+    [3, 4]
+];
+
+console.log(matrix[1][1]);
+
